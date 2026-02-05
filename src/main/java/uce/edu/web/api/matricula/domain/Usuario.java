d21fe35de5b456d9d935109060fc2db3cb4a20df
JhonnyNinabanda <jeninabandap@uce.edu.ec>
2026-02-05 12:47:33 -0500

    TAREA 7: OAuth 2.0

---
+++ src/main/java/uce/edu/web/api/matricula/domain/Usuario.java
@@ -0,0 +1,24 @@
+package uce.edu.web.api.matricula.domain;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String username;
+    public String password;
+    public String role;
+
+    public static Usuario findByUsername(String username) {
+        return find("username", username).firstResult();
+    }
+}   
